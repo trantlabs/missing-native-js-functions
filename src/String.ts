7b3855cf63a3b2606f16e5115dc75e3ec6da790f
xnacly <matteogropp@gmail.com>
2021-01-10 13:42:39 +0100

    added intellisense decs to new methods (string)

--- src/String.ts
+++ src/String.ts
@@ -107,6 +107,10 @@ declare global {
 		capitalize(): string;
 		replaceAll(search: string, replace: string): string;
 		similarity(compare: string): number;
+		join(iterate: string[]): string;
+		partition(separator: string): string[];
+		toNumber(): number;
+		toBigInt(): bigint;
 	}
 }
 export {};
