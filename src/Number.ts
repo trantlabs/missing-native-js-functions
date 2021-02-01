91953e9240ac47b95b1437269e658413d0d06657
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-01 19:12:55 +0100

    :sparkles: Number.toInt()

---
+++ src/Number.ts
@@ -0,0 +1,13 @@
+import { define } from "./Util";
+
+define(Number.prototype, {
+	toInt: function () {
+		return Math.floor(this);
+	},
+});
+
+declare global {
+	interface Number {
+		toInt(): number;
+	}
+}
