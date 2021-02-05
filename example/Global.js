1f5d3085162ced757c70c8bc3fb375319c193503
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-05 17:13:25 +0100

    :sparkles: setIntervalNow

--- example/Global.js
+++ example/Global.js
@@ -4,3 +4,11 @@ const convert = "this string will be base64 encoded";
 const converted = btoa("this string was base64 encoded");
 console.log(convert, btoa(convert));
 console.log(atob(converted), converted);
+
+const interval = setIntervalNow(() => {
+	console.log("this function will be called immediately and every 1000ms");
+}, 1000);
+
+setTimeout(() => {
+	clearInterval(interval);
+}, 1500);
--- src/Global.ts
+++ src/Global.ts
@@ -8,6 +8,13 @@ try {
 
 if (!Global.atob) Global.atob = (data: string) => Buffer.from(data, "base64").toString("utf8");
 if (!Global.btoa) Global.btoa = (data: string) => Buffer.from(data.toString(), "utf8").toString("base64");
+if (!Global.setIntervalNow) {
+	Global.setIntervalNow = function (callback: Function, milliseconds?: number, ...args: any[]) {
+		const func = callback.bind(this, ...args);
+		func();
+		return setInterval(func, milliseconds);
+	};
+}
 
 declare global {
 	/**
@@ -20,5 +27,7 @@ declare global {
 	 * @param {string} data UTF-8 string that should get encoded
 	 */
 	function btoa(data: string): string;
+
+	function setIntervalNow(callback: Function, milliseconds?: number, ...args: any[]): number | NodeJS.Timeout;
 }
 export {};
