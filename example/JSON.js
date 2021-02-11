151d81c7cfa134e89dc388a2cbf71c4a8788cb38
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-11 10:48:37 +0100

    :pencil: added JSON example comments

--- example/JSON.js
+++ example/JSON.js
@@ -4,3 +4,5 @@ const bigint = { test: 234986234982634923423n };
 bigint.circular = { obj: bigint };
 
 console.log("stringify bigint and circular object:", JSON.stringify(bigint));
+// -> {"test":"234986234982634923423","circular":{}}
+// -> bigint.circular.obj is circular, so it will be completly removed
