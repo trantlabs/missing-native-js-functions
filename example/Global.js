828a3c10848fc1596c2d755c4022b8074934fc8b
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-11 10:48:21 +0100

    :pencil: added Global example comments

--- example/Global.js
+++ example/Global.js
@@ -2,13 +2,19 @@ require("../dist/Global");
 
 const convert = "this string will be base64 encoded";
 const converted = btoa("this string was base64 encoded");
+// -> converted: dGhpcyBzdHJpbmcgd2lsbCBiZSBiYXNlNjQgZW5jb2RlZA==
+
 console.log(convert, btoa(convert));
+// -> this string will be base64 encoded , dGhpcyBzdHJpbmcgd2lsbCBiZSBiYXNlNjQgZW5jb2RlZA==
+
 console.log(atob(converted), converted);
+// -> this string was base64 encoded dGhpcyBzdHJpbmcgd2FzIGJhc2U2NCBlbmNvZGVk
 
 const interval = setIntervalNow(() => {
 	console.log("this function will be called immediately and every 1000ms");
 }, 1000);
 
+// ignore -> will clear the interval
 setTimeout(() => {
 	clearInterval(interval);
 }, 1500);
