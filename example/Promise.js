fd45d9f4377acaa82315b61578c5dbc3cd6c7531
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-02-11 10:56:30 +0100

    :pencil: added comments to String example

--- example/Promise.js
+++ example/Promise.js
@@ -3,3 +3,4 @@ require("../dist/Promise");
 new Promise((res, rej) => {
 	rej("Promised rejected, but caught in console.error");
 }).caught();
+// .caught() will add a .catch listener that logs the error in the error
