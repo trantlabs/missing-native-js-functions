19472643cf6d493453a4bfc091523717f32052c7
Conner Bachmann <bac@christmann.info>
2022-10-06 21:34:27 +0200

    feat(date): added setTimezone function

--- src/Date.ts
+++ src/Date.ts
@@ -1,14 +1,22 @@
 import { define } from "./Util";
-define(Date, {
+define(Date.prototype, {
 	nowSeconds: function () {
 		return Math.floor(Date.now() / 1000);
 	},
+	setTimezone: function (timezone: string) {
+		return new Date(
+			this.toLocaleString("en-US", {
+				timezone,
+			})
+		);
+	}
 });
 
 declare global {
 	//@ts-ignore
 	interface DateConstructor {
 		nowSeconds(): number;
+		setTimezone(): Date;
 	}
 }
 
