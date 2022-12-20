7c2730d0f6fc27866ea035a196e9b0179119d0e3
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-12-20 10:52:30 +0100

    doc: add docs for Date

--- src/Date.ts
+++ src/Date.ts
@@ -1,8 +1,14 @@
 import {define} from './Util'
 define(Date.prototype, {
+    /**
+    * Returns the current timestamp as its representation in seconds
+    */
     nowSeconds: function () {
         return Math.floor(Date.now() / 1000)
     },
+    /**
+    * specifies the timezone for the current date
+    */
     setTimezone: function (timezone: string) {
         return new Date(
             this.toLocaleString('en-US', {
@@ -10,6 +16,9 @@ define(Date.prototype, {
             })
         )
     },
+    /**
+    * checks if the date is in the past, compares with current date
+    */
     isInPast: function () {
         const today = new Date()
         today.setHours(0, 0, 0, 0)
