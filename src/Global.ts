78fa8b9f65db26ddbdee882b1d3ba8d57bad0b7e
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-12-21 12:11:47 +0100

    doc: add examples to Global.ts and Promise.ts

--- src/Global.ts
+++ src/Global.ts
@@ -21,11 +21,15 @@ declare global {
 	/**
 	 * Converts a Base64 encoded string
 	 * @param {string} data Base64 encoded string
+	 * @example
+	 * atob("dGhpcyBzdHJpbmcgd2FzIGJhc2U2NCBlbmNvZGVk"); // returns: this string was base64 encoded
 	 */
 	function atob(data: string): string;
 	/**
 	 * Converts a string to a Base64 encoded string
 	 * @param {string} data UTF-8 string that should get encoded
+	 * @example
+	 * btoa("this string will be base64 encoded"); // returns: dGhpcyBzdHJpbmcgd2lsbCBiZSBiYXNlNjQgZW5jb2RlZA==
 	 */
 	function btoa(data: string): string;
 
@@ -34,13 +38,21 @@ declare global {
 	 * @param {callback} callback a function to be executed every delay milliseconds. The first execution happens after delay milliseconds.
 	 * @param {milliseconds} milliseconds The time, in milliseconds (thousandths of a second), the timer should delay in between executions of the specified function or code.
 	 * @param {args} args Additional arguments which are passed through to the function specified by func once the timer expires.
-	 * https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+	 * @example
+	 * setInterval((a, b) => {
+	 *  console.log(a, b)
+	 * }, 500, "param 1", "param 2"); // prints param 1 param 2 every 500 ms
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/setInterval
 	 */
 	function setIntervalNow(callback: Function, milliseconds?: number, ...args: any[]): number | NodeJS.Timeout;
 
 	/**
 	 * Returns a promise that resolves after the specified milliseconds
 	 * @param {ms} ms The time, in milliseconds (thousandths of a second), the function should delay the execution of the program.
+	 * @example
+	 * (async () => {
+	 *  await sleep(3000); // waits 3 seconds
+	 * })();
 	 */
 	function sleep(ms: number): Promise<void>;
 }
--- src/Promise.ts
+++ src/Promise.ts
@@ -12,6 +12,10 @@ declare global {
 	interface Promise<T> {
 		/**
 		 * Catches all errors in the console without the need to specify a function, similar to `promise.catch(console.error)`
+		 * @example
+		 * new Promise((res, rej) => {
+		 *	rej("Promised rejected, but caught in console.error");
+		 *}).caught(); // will not throw the promise, but log the error in the console
 		 */
 		caught(): this;
 	}
